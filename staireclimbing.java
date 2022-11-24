7f0faa582035b583b2e424774cf4130a3ee9a55d
Yogesh panchal <93254488+Yogesh82001@users.noreply.github.com>
2022-11-24 19:42:59 +0530

    Add files via upload

---
+++ staireclimbing.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class staireclimbing{
+        static int fib(int n)
+    {
+        if (n <= 1)
+            return n;
+        return fib(n - 1) + fib(n - 2);
+    }
+ 
+    
+    static int countWays(int s)
+    {
+        return fib(s + 1);
+    }
+    public static void main(String args[])
+    {
+        System.out.println("enter the value of stair : ");
+        Scanner sc=new Scanner(System.in);
+        int s = sc.nextInt();
+        System.out.println("Number of ways = " + countWays(s));
+    }
+} 
+    
+
