ffcd14a42da0398da3facd94bcb660bd39822517
Yogesh panchal <93254488+Yogesh82001@users.noreply.github.com>
2023-02-19 14:17:38 +0530

    Add files via upload

--- secondMaxElement.java
+++ secondMaxElement.java
@@ -1,23 +1,57 @@
 package homeworkrdec;
-
+import java.io.*;
 public class secondMaxElement {
-    static void Secondmax(int arr[],int n){
-        int count=0;
-        int max=arr[0];
-        for(int i=1;i<n;i++){
-            if(arr[i]>max){
-                max=arr[i];
-                count++;
-            }
-            if(count==1)
-            break;
-        }
-        System.out.println("Second max element is "+max);
-    }
-    public static void main(String[] args) {
-        int arr[]={10,5,20,2,50};
-        int n=arr.length;
-        Secondmax(arr, n);
-    }
-    
+    // JAVA Code for Find Second largest
+// element in an array
+
+
+
+
+	/* Function to print the second largest
+	elements */
+	public static void print2largest(int arr[],
+									int arr_size)
+	{
+		int i, first, second;
+
+		/* There should be atleast two elements */
+		if (arr_size < 2) {
+			System.out.print(" Invalid Input ");
+			return;
+		}
+
+		first = second = Integer.MIN_VALUE;
+		for (i = 0; i < arr_size; i++) {
+			/* If current element is greater than
+			first then update both first and second */
+			if (arr[i] > first) {
+				second = first;
+				first = arr[i];
+			}
+
+			/* If arr[i] is in between first and
+			second then update second */
+			else if (arr[i] > second && arr[i] != first)
+				second = arr[i];
+		}
+
+		if (second == Integer.MIN_VALUE)
+			System.out.print("There is no second largest"
+							+ " element\n");
+		else
+			System.out.print("The second largest element"
+							+ " is " + second);
+	}
+
+	/* Driver program to test above function */
+	public static void main(String[] args)
+	{
+		int arr[] = { 1,2,3,4 };
+		int n = arr.length;
+		print2largest(arr, n);
+	}
 }
+// This code is contributed by Arnav Kr. Mandal.
+
+    
+
